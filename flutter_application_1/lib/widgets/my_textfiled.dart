07d9fe359d10cb0a6ee09179a65fc58cd73900b1
LOW JIA QING <148974474+LOWJQ@users.noreply.github.com>
2025-06-28 06:14:04 +0800

    Update my_textfiled.dart

--- flutter_application_1/lib/widgets/my_textfiled.dart
+++ flutter_application_1/lib/widgets/my_textfiled.dart
@@ -28,34 +28,37 @@ class _MyTextFieldState extends State<MyTextField> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25.0),
-      child: TextField(
-        controller: widget.controller,
-        obscureText: widget.obscureText ? _obscureText : false,
-        decoration: InputDecoration(
-          enabledBorder: const OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.white),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.grey),
-          ),
-          fillColor: Colors.grey.shade200,
-          filled: true,
-          hintText: widget.hintText,
-          hintStyle: TextStyle(color: Colors.grey[500]),
-          suffixIcon: widget.obscureText
-              ? IconButton(
-            icon: Icon(
-              _obscureText ? Icons.visibility_off : Icons.visibility,
-              color: Colors.grey,
+      padding: const EdgeInsets.symmetric(horizontal: 25.0), // consistent width
+      child: SizedBox(
+        width: double.infinity,
+        child: TextField(
+          controller: widget.controller,
+          obscureText: widget.obscureText ? _obscureText : false,
+          decoration: InputDecoration(
+            enabledBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
             ),
-            onPressed: () {
-              setState(() {
-                _obscureText = !_obscureText;
-              });
-            },
-          )
-              : null,
+            focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+            fillColor: Colors.grey.shade200,
+            filled: true,
+            hintText: widget.hintText,
+            hintStyle: TextStyle(color: Colors.grey[500]),
+            suffixIcon: widget.obscureText
+                ? IconButton(
+              icon: Icon(
+                _obscureText ? Icons.visibility_off : Icons.visibility,
+                color: Colors.grey,
+              ),
+              onPressed: () {
+                setState(() {
+                  _obscureText = !_obscureText;
+                });
+              },
+            )
+                : null,
+          ),
         ),
       ),
     );
