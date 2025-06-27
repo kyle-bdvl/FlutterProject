05580b9d1a1b8f4f3f0aea7953b8486b3dd764f2
LOW JIA QING <148974474+LOWJQ@users.noreply.github.com>
2025-06-28 05:23:30 +0800

    Update settings.gradle.kts

--- flutter_application_1/android/settings.gradle.kts
+++ flutter_application_1/android/settings.gradle.kts
@@ -9,6 +9,11 @@ pluginManagement {
 
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
+    plugins {
+        id("com.android.application") version "8.7.0"
+        id("org.jetbrains.kotlin.android") version "2.1.0"
+    }
+
     repositories {
         google()
         mavenCentral()
@@ -16,10 +21,11 @@ pluginManagement {
     }
 }
 
+
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.7.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
 
 include(":app")
