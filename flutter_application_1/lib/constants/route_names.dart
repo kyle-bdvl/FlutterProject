268613204ec47a8a8f27be8bd9055c8c5e9095fb
unknown <225328@student.upm.edu.my>
2025-06-28 04:03:54 +0800

    Move project into flutter_application_1 subfolder

--- flutter_application_1/lib/constants/route_names.dart
+++
@@ -1,4 +0,0 @@
-const String routeLogin = '/login';
-const String routeForgotPassword = '/forgot-password';
-const String routeVerifyOtp = '/verify-otp';
-const String routeResetPassword = '/reset-password';
