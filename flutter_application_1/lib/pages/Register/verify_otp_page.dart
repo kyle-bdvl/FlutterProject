6c7829b6f757500d2779254531cae2595912bf6d
LOW JIA QING <148974474+LOWJQ@users.noreply.github.com>
2025-06-28 06:14:35 +0800

    Update and rename flutter_application_1/lib/pages/verify_otp_page.dart to flutter_application_1/lib/pages/Register/verify_otp_page.dart

---
+++ flutter_application_1/lib/pages/Register/verify_otp_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class MyTextField extends StatefulWidget {
+  final TextEditingController controller;
+  final String hintText;
+  final bool obscureText;
+
+  const MyTextField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+  });
+
+  @override
+  State<MyTextField> createState() => _MyTextFieldState();
+}
+
+class _MyTextFieldState extends State<MyTextField> {
+  late bool _obscureText;
+
+  @override
+  void initState() {
+    super.initState();
+    _obscureText = widget.obscureText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0), // consistent width
+      child: SizedBox(
+        width: double.infinity,
+        child: TextField(
+          controller: widget.controller,
+          obscureText: widget.obscureText ? _obscureText : false,
+          decoration: InputDecoration(
+            enabledBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+            focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+            fillColor: Colors.grey.shade200,
+            filled: true,
+            hintText: widget.hintText,
+            hintStyle: TextStyle(color: Colors.grey[500]),
+            suffixIcon: widget.obscureText
+                ? IconButton(
+              icon: Icon(
+                _obscureText ? Icons.visibility_off : Icons.visibility,
+                color: Colors.grey,
+              ),
+              onPressed: () {
+                setState(() {
+                  _obscureText = !_obscureText;
+                });
+              },
+            )
+                : null,
+          ),
+        ),
+      ),
+    );
+  }
+}
