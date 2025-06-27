ac68e2eda2ec19e0e959606b8fe8dc08111ec926
LOW JIA QING <148974474+LOWJQ@users.noreply.github.com>
2025-06-28 05:32:40 +0800

    Update my_button.dart

--- flutter_application_1/lib/widgets/my_button.dart
+++ flutter_application_1/lib/widgets/my_button.dart
@@ -2,26 +2,27 @@ import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
   final Function()? onTap;
-
   final String text;
 
   const MyButton({super.key, required this.onTap, this.text = "Sign In"});
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        padding: const EdgeInsets.all(25),
-        margin: const EdgeInsets.symmetric(horizontal: 25),
-        decoration: BoxDecoration(
-          color: Colors.black,
-          borderRadius: BorderRadius.circular(8),
-        ),
-        child: Center(
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0), // Match MyTextField
+      child: SizedBox(
+        height: 60,
+        width: double.infinity,
+        child: ElevatedButton(
+          onPressed: onTap,
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.black,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
           child: Text(
-            text, // <-- Use the text property
-
+            text,
             style: const TextStyle(
               color: Colors.white,
               fontWeight: FontWeight.bold,
