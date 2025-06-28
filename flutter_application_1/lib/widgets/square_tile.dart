8440c4440a1b422002e2c6bf733abc820f705c62
LOW JIA QING <148974474+LOWJQ@users.noreply.github.com>
2025-06-28 12:17:10 +0800

    Update square_tile.dart

--- flutter_application_1/lib/widgets/square_tile.dart
+++ flutter_application_1/lib/widgets/square_tile.dart
@@ -2,18 +2,20 @@ import 'package:flutter/material.dart';
 
 class SquareTile extends StatelessWidget {
   final String imagePath;
-  const SquareTile({super.key, required this.imagePath});
+  final double size;
+
+  const SquareTile({super.key, required this.imagePath, this.size = 50});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(20),
+      padding: const EdgeInsets.all(20),
       decoration: BoxDecoration(
         border: Border.all(color: Colors.white),
-        borderRadius: BorderRadius.circular(16),
         color: Colors.grey[200],
+        borderRadius: BorderRadius.circular(8),
       ),
-      child: Image.asset(imagePath, height: 50, width: 50, fit: BoxFit.contain),
+      child: Image.asset(imagePath, height: size, width: size),
     );
   }
 }
