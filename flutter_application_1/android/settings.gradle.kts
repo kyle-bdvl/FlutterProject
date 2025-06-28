c57a55a9e16a30648ad46c1bec161cc166ac9ee5
ewingingit <ewinghogawing@gmail.com>
2025-06-28 18:48:45 +0800

    connected to firebase

--- flutter_application_1/android/settings.gradle.kts
+++ flutter_application_1/android/settings.gradle.kts
@@ -11,6 +11,9 @@ pluginManagement {
 
     plugins {
         id("com.android.application") version "8.7.0"
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services") version("4.3.15") apply false
+    // END: FlutterFire Configuration
         id("org.jetbrains.kotlin.android") version "2.1.0"
     }
 
